f7e068c79552737a962d3721b9d0d6b339313f1e
HariTestingGH <hari.charan@gradehive.ai>
2025-09-17 10:38:11 +0530

    cleaning up

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use('/api/translate', translateRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
